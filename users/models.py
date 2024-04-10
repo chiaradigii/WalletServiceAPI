e3c849924a1c4984e0c87a1c409023865f3e3c77
Chiara Di Giannantonio <chiaradigi2@gmail.com>
2024-04-10 20:11:09 +0200

    Setting User model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,51 @@
-from django.db import models
+"""
+This model will serve as the foundation for handling both client and commerce users.
+Different types of users are considered, with endpoints for performing the following operations:
 
-# Create your models here.
+Client: must be able to register for the service, create one or more wallet accounts (which will be identified by a unique token), recharge the balance of their accounts, check the status of their accounts, and list the transactions of their accounts.
+Merchant: must be able to register for the service, create a wallet account, charge a client's wallet account using the account's token as a reference, and list the transactions of their merchant account.
+"""
+
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Users must have an email address')
+        if not password:
+            raise ValueError('Users must have a password')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        """ Create a superuser with email and password """
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return self.is_superuser
+
+    def has_module_perms(self, app_label):
+        return self.is_superuser
+
